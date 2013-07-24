84a3e4073cbcbdfcc79d0f1ac4abc75875fbff3a
Chris Plock <cplock@igamer.plock.org>
2013-07-23 22:06:08 -0700

    works as far as i can tell
    backed out of the close_window_helper, and instead generally handling
    apple only return from stopping child processes

--- homebrew formulas/multitail.rb
+++ homebrew formulas/multitail.rb
@@ -50,80 +50,18 @@ diff -u ../multitail-5.2.13-orig/error.c ./error.c
          fprintf(stderr, "Execution path:\n");
          for(index=0; index<trace_size; ++index)
                  fprintf(stderr, "\t%d %s\n", index, messages[index]);
-diff -u ../multitail-5.2.13-orig/mt.c ./mt.c
---- ../multitail-5.2.13-orig/mt.c	2013-06-19 11:22:09.000000000 -0700
-+++ ./mt.c	2013-07-12 17:07:02.000000000 -0700
-@@ -1882,7 +1882,7 @@
- 	lb[f_index].maxnlines = maxnlines;
- }
- 
--char close_window(int winnr, proginfo *cur)
-+char close_window_helper(int winnr, proginfo *cur, mybool_t stopProcess)
- {
- 	if (winnr == terminal_main_index)
- 	{
-@@ -1891,7 +1891,10 @@
- 	}
- 
- 	/* make sure it is really gone */
--	stop_process(cur -> pid);
-+	if (stopProcess == 1)
-+	{
-+		stop_process(cur -> pid);
-+	}
- 
- 	/* restart window? */
- 	if (cur -> restart.restart >= 0)
-@@ -1978,6 +1981,17 @@
- 	}
- }
- 
-+/* OSX: use with false if we do not want to send TERM signals to the process, like if we got the pid from a wait already */
-+char close_window_for_dead_process(int winnr, proginfo *cur) {
-+	return close_window_helper(winnr, cur, 0);
-+}
-+char close_window_and_stop_process(int winnr, proginfo *cur) {
-+	return close_window_helper(winnr, cur, 1 );
-+}
-+char close_window(int winnr, proginfo *cur) {
-+	return close_window_helper(winnr, cur, 1 );
-+}
-+
- void do_restart_window(proginfo *cur)
- {
- 	/* stop process */
-@@ -2874,7 +2888,7 @@
- 				/* did it exit? */
- 				if (died_proc == cur -> pid && cur -> wt != WT_STDIN && cur -> wt != WT_SOCKET)
- 				{
--					int refresh_info = close_window(f_index, cur);
-+					int refresh_info = close_window_for_dead_process(f_index, cur);
- 
- 					found = a_window_got_closed = 1;
- 
-@@ -3590,6 +3604,9 @@
- 
- 			if (check_for_died_processes())
- 			{
-+				if (!do_not_close_closed_windows && terminal_index == NULL) {
-+					do_exit();
-+				}
- 				c = -1;
- 				break;
- 			}
-@@ -3607,6 +3624,9 @@
- 
- 		if (deleted_entry_in_array >= 0)
- 		{
-+			if (!do_not_close_closed_windows && terminal_index == NULL) {
-+				do_exit();
-+			}
- 			c = -1;
- 			break;
- 		}
 diff -u ../multitail-5.2.13-orig/utils.c ./utils.c
 --- ../multitail-5.2.13-orig/utils.c	2013-06-19 11:22:09.000000000 -0700
-+++ ./utils.c	2013-07-12 17:12:11.000000000 -0700
++++ ./utils.c	2013-07-15 09:29:52.000000000 -0700
+@@ -27,7 +27,7 @@
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <fcntl.h>
+-
++     
+ #include "version.h"
+ #include "error.h"
+ #include "mem.h"
 @@ -184,10 +184,14 @@
  {
  	assert(pid > 1);
@@ -153,10 +91,10 @@ diff -u ../multitail-5.2.13-orig/utils.c ./utils.c
 -				error_exit("Problem stopping child process with PID %d (SIGKILL).\n", pid);
 +			if (errno == EINVAL)
 +				error_exit("Bad signal when stopping child process with PID %d (SIGKILL).\n", pid);
-+			#ifndef __APPLE__
-+					else if (errno == EPERM)
-+						error_exit("Bad permissions stopping child process with PID %d (SIGTERM).\n", pid);
-+			#endif
++#ifndef __APPLE__
++			else if (errno == EPERM)
++				error_exit("Bad permissions stopping child process with PID %d (SIGKILL).\n", pid);
++#endif
  		}
  	}
 -	else if (errno != ESRCH)
@@ -173,3 +111,26 @@ diff -u ../multitail-5.2.13-orig/utils.c ./utils.c
  	/* wait for the last remainder of the died process to go away,
  	 * otherwhise we'll find zombies on our way
  	 */
+diff -u ../multitail-5.2.13-orig/mt.c ./mt.c
+--- ../multitail-5.2.13-orig/mt.c	2013-06-19 11:22:09.000000000 -0700
++++ ./mt.c	2013-07-12 17:07:02.000000000 -0700
+@@ -3590,6 +3604,9 @@
+ 
+ 			if (check_for_died_processes())
+ 			{
++				if (!do_not_close_closed_windows && terminal_index == NULL) {
++					do_exit();
++				}
+ 				c = -1;
+ 				break;
+ 			}
+@@ -3607,6 +3624,9 @@
+ 
+ 		if (deleted_entry_in_array >= 0)
+ 		{
++			if (!do_not_close_closed_windows && terminal_index == NULL) {
++				do_exit();
++			}
+ 			c = -1;
+ 			break;
+ 		}
